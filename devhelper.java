36e75560c98c6384d8327daaf4b1f0a380c3bd31
Max Rydahl Andersen <max.andersen@gmail.com>
2023-12-14 12:48:58 +0100

    initial commit

--- devhelper.java
+++ devhelper.java
@@ -17,6 +17,11 @@
 //Q:CONFIG quarkus.langchain4j.openai.chat-model.model-name=gpt-4
 //Q:CONFIG quarkus.langchain4j.openai.timeout=60s
 
+// To run in devmode use this command:
+// `jbang --fresh -Dquarkus.dev -Dquarkus.console.enabled=false devhelper.java`
+// This ensures fresh build, dev mode enabled and don't grab the console so
+// you can ask questions in the terminal.
+
 import static io.quarkus.logging.Log.info;
 import static java.lang.System.out;
 
@@ -38,13 +43,44 @@ import jakarta.inject.Inject;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
-@Command(mixinStandardHelpOptions = true, version = "0.1", 
-		header = "Have fun with Quarkus and LangChain4j", 
-		description = "Uses Quarkus LangChain4j to help understand and update your code.")
+@Command(mixinStandardHelpOptions = true, version = "0.1", header = "Have fun with Quarkus and LangChain4j", description = "Uses Quarkus LangChain4j to help understand and update your code.")
 public class devhelper implements Runnable {
 
 	@Parameters(description = "The question to answer", defaultValue = "What can you tell me about my project?")
 	String question;
+
+	@RegisterAiService(tools = FileManager.class)
+	public interface ProjectHelper {
+
+		@SystemMessage("""
+				You are to help a developer understand his project. You can ask him questions or query the files in his project to get more info.
+				If he asks you to modify a file, you can do so by calling the 'updateFile' tool.
+				""")
+		@UserMessage("{question}")
+		String ask(String question);
+	}
+
+	@Inject
+	ProjectHelper ai;
+
+	@ActivateRequestContext
+	public void run() {
+
+		while (true) {
+			var scanner = new Scanner(System.in);
+			if (question == null || question.isBlank()) {
+				out.println("Please enter your question:");
+				question = scanner.nextLine();
+				if (Set.of("exit", "quit").contains(question.toLowerCase())) {
+					break;
+				}
+			}
+			out.println("Thinking...");
+			String answer = ai.ask(question);
+			out.println("Answer: " + answer);
+			question = null;
+		}
+	}
 
 	@ApplicationScoped
 	static class FileManager {
@@ -56,12 +92,11 @@ public class devhelper implements Runnable {
 				""")
 		public List<String> getFiles(
 				@P("""
-					The name of the directory relatively to the root of the project.
-					Is a simple string. Use '/' to get the root directory.
-					""") String directory)
+						The name of the directory relatively to the root of the project.
+						Is a simple string. Use '/' to get the root directory.
+						""") String directory)
 				throws IOException {
 			directory = handleDir(directory);
-
 			info("Getting files in directory " + directory);
 
 			var files = Files.list(Paths.get(directory)).map(p -> p.toString()).toList();
@@ -72,9 +107,9 @@ public class devhelper implements Runnable {
 		@Tool("Get the content of a file")
 		public String getFile(
 				@P("""
-					Read the content of a file. 
-					The parameter is a single string relative to the project root.
-					""") String filename)
+						Read the content of a file.
+						The parameter is a single string relative to the project root.
+						""") String filename)
 				throws IOException {
 			filename = handleDir(filename);
 
@@ -91,12 +126,12 @@ public class devhelper implements Runnable {
 
 			filename = handleDir(filename);
 
-			userConfirmation("Update file '" + filename + "' with content:" + content, 
-							 "Are you sure you want to update the content of " + filename + "? (yes/no)");
-			
+			userConfirmation("Update file '" + filename + "' with content:" + content,
+					"Are you sure you want to update the content of " + filename + "? (yes/no)");
+
 			info("Updating content of file '" + filename + "'");
 
-			Files.createDirectories(Paths.get(filename).toAbsolutePath().getParent());
+			Files.createDirectories(Paths.get(filename).getParent());
 			Files.writeString(Paths.get(filename), content);
 		}
 
@@ -106,31 +141,27 @@ public class devhelper implements Runnable {
 
 			filename = handleDir(filename);
 
-			userConfirmation("Delete file '" + filename, 
-							 "Are you sure you want to delete " + filename + "? (yes/no)");
+			userConfirmation("Delete file '" + filename,
+					"Are you sure you want to delete " + filename + "? (yes/no)");
 
 			info("Removing file '" + filename + "'");
 
 			Files.delete(Paths.get(filename));
 		}
 
+		// Check for '..' in the path to avoid accessing files outside of the project.
+		// Make request to / be the same as a request to . (root of current directory)
 		private String handleDir(String directory) {
 			if (directory.contains("..")) {
 				throw new IllegalArgumentException(
 						"The path cannot contain '..' as it would allow to access files outside of the project.");
 			}
-			if (directory == null || directory.isBlank()) {
-				directory = "/";
-			}
-
-			if (directory.startsWith("/")) {
-				directory = directory.substring(1);
-			}
-			return directory;
+			directory = (directory == null || directory.isBlank())? "/" : directory;
+			return (directory.startsWith("/"))? directory.substring(1) : directory;
 		}
 
 		private void userConfirmation(String message, String confirmationQuestion) throws IOException {
-			Scanner scanner = new Scanner(System.in); 
+			Scanner scanner = new Scanner(System.in);
 			out.println(message);
 			out.println(confirmationQuestion);
 			String response = scanner.nextLine();
@@ -139,42 +170,4 @@ public class devhelper implements Runnable {
 			}
 		}
 	}
-
-	@RegisterAiService(tools = FileManager.class)
-	public interface ProjectHelper {
-
-		@SystemMessage("""
-				You are to help a developer understand his project. You can ask him questions or query the files in his project to get more info.
-				If he asks you to modify a file, you can do so by calling the 'updateFile' tool.
-				""")
-		@UserMessage("{question}")
-		String ask(String question);
-	}
-
-	@Inject
-	ProjectHelper ai;
-
-	@Override
-	@ActivateRequestContext
-	public void run() {
-
-		
-		while (true) {
-			var scanner = new Scanner(System.in);
-                if (question == null || question.isBlank()) {
-                	out.println("Please enter your question:");
-                	question = scanner.nextLine();
-                	if (Set.of("exit", "quit").contains(question.toLowerCase())) {
-                		break;
-                	}
-                }
-		
-			out.println("Thinking...");
-			String answer = ai.ask(question);
-			out.println("Answer: " + answer);
-			question = null;
-		}
-
-	}
-
 }
